e6c466468334100c38630e142463d5a30f89c717
Nate Goldman <ungoldman@gmail.com>
2017-09-07 21:44:33 -0700

    feat: add api 🙃

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
 var index = require('./routes/index')
-var users = require('./routes/users')
+var api = require('./routes/api')
 
 var app = express()
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // routes
 app.use('/', index)
-app.use('/users', users)
+app.use('/api', api)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
